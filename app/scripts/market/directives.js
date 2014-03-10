85c74e0c6cb6779924a8ce818fcd2f250f54a9d7
elliotcw <williams.elliot@gmail.com>
2014-03-10 00:10:45 +0000

    broke out depth from market view

--- app/scripts/market/directives.js
+++ app/scripts/market/directives.js
@@ -1,79 +1,74 @@
 define(function (require) {
     'use strict';
 
-    var _ = require('lodash');
     var angular = require('angular');
-    var depthTemplate = require('text!./depth-template.html');
+    var depthTemplate = require('text!./templates/depth.html');
+    var marketTemplate = require('text!./templates/market.html');
 
     angular.module('market.directives', ['market.services'])
-        .directive('depth', function (depthService, $timeout) {
+        .directive('market', function (depthService, $timeout) {
             return {
                 restrict: 'E',
-                template: depthTemplate,
+                template: marketTemplate,
                 controller: function ($scope, $element) {
+
+                    $scope.bids = [];
+                    $scope.asks = [];
+
+                    $scope.groupedBids = [];
+                    $scope.groupedAsks = [];
+
                     $scope.close = function () {
+                        if ($scope.pollTimeout) {
+                            $timeout.cancel($scope.pollTimeout);
+                        }
                         $element.remove();
                     };
 
-                    $scope.loading = false;
-
-                    $scope.$watch('instrumentCode', function () {
-                        $scope.loading = true;
-                        $scope.fetchData().then(function () {
-                            $scope.loading = false;
-                        });
+                    $scope.$watch('instrumentCode', function (newValue) {
+                        if (newValue) {
+                            $scope.loading = true;
+                            $scope.fetchData().then(function () {
+                                $scope.loading = false;
+                            });
+                        }
                     });
 
                     $scope.fetchData = function () {
-                        return depthService($scope.instrumentCode).then(function (response) {                            
-                            $scope.bids = response.data.bids;
-                            $scope.asks = response.data.asks;
+                        return depthService($scope.instrumentCode).then(function (data) {
+                            //  success callback
+                            $scope.bids = data.bids || [];
+                            $scope.asks = data.asks || [];
 
-                            $scope.groupedBids = _.groupBy($scope.bids, function (bid) {
-                                return Math.round(bid.price * 100) / 100;
-                            });
+                            $scope.groupedBids = data.groupedBids || [];
+                            $scope.groupedAsks = data.groupedAsks || [];
 
-                            $scope.groupedBids = _.map($scope.groupedBids, function (group) {
-                                var unfilled = _.reduce(group, function (sum, bid) {
-                                    sum = sum.unfilled + bid.unfilled;
-                                    return {unfilled: sum};
-                                }).unfilled;
-                                var isMyOrder = _.filter(group, function (bid) {
-                                    return bid.isMyOrder;
-                                }).length;
-                                return {price: group[0].price, unfilled: unfilled, isMyOrder: isMyOrder};
-                            });
-
-                            $scope.groupedAsks = _.groupBy($scope.asks, function (ask) {
-                                return Math.round(ask.price * 100) / 100;
-                            });
-
-                            $scope.groupedAsks = _.map($scope.groupedAsks, function (group) {
-                                var unfilled = _.reduce(group, function (sum, ask) {
-                                    sum = sum.unfilled + ask.unfilled;
-                                    return {unfilled: sum};
-                                }).unfilled;
-                                var isMyOrder = _.filter(group, function (ask) {
-                                    return ask.isMyOrder;
-                                }).length;
-                                return {price: group[0].price, unfilled: unfilled, isMyOrder: isMyOrder};
-                            });
-
-                            if ($scope.poll) {
-                                $timeout.cancel($scope.poll);
+                            if ($scope.pollTimeout) {
+                                $timeout.cancel($scope.pollTimeout);
                             }
 
-                            $scope.poll = $timeout(function () {
+                            $scope.pollTimeout = $timeout(function () {
                                 $scope.fetchData();
                             }, 1000);
                         }, function () {
+                            // error callback
                             console.log(arguments);
-                            $scope.asks = [];
-                            $scope.bids = [];
-                            $scope.groupedAsks = [];
-                            $scope.groupedBids = [];
                         });
-                    };                    
+                    };
+                }
+            };
+        })
+        .directive('depth', function () {
+            return {
+                restrict: 'E',
+                template: depthTemplate,
+                scope: {
+                    'orders': '=',
+                    'side': '@'
+                },
+                controller: function ($scope, $filter) {
+                    var expression = ($scope.side === 'bid') ? ['-price', '-enteredTimestamp'] : ['price', '-enteredTimestamp'];
+                    $scope.orders = $filter('orderBy')($scope.orders, expression);
                 }
             };
         });
